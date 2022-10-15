44800db18c3376b1336be2f2dca045568423d256
Maxski <ma.ad.jank@gmail.com>
2022-10-15 16:31:24 +0100

    new

--- JP/kobold_ai.py
+++ JP/kobold_ai.py
@@ -7,9 +7,25 @@ def generate_response(text_prompt, api_link):
     url = api_link + "generate"
     request_body = {"prompt" : text_prompt}
 
-    text_response = requests.post(url, json = request_body)
+    text_response = requests.post(url, json = request_body).json()['results'][0]['text'] #returns a string
 
-    return text_response.json()['results'][0]['text'] #returns a string
+    found = False
+    i = 0
+    while found == False and i < len(text_response):
+        if text_response[-i-1] == ',' or text_response[-i-1] == '.':
+            
+            end_of_sentence = len(text_response) - i
+            print(text_response)
+            text_response = text_response[:end_of_sentence]
+            print(text_response)   
+
+            if text_response[-1] == ',':
+                text_response = text_response[:-1] + "."
+                print(text_response)
+            found = True
+        i+=1
+
+    return text_response
 
 def process_prompt(text_prompt):
 
@@ -28,3 +44,5 @@ def process_prompt(text_prompt):
         text_prompt = "You " + text_prompt
 
     return text_prompt
+
+generate_response("You fight the Batman.", "http://nasty-ravens-stare-104-198-213-42.loca.lt/api/v1/")
--- JP/stable_diffusion.py
+++ JP/stable_diffusion.py
@@ -2,7 +2,7 @@ import replicate
 #pip install replicate
 #export REPLICATE_API_TOKEN=ec2408b84a5d6975bf214c713cd71c84d4c22a8b
 
-def getImage(image_prompt):
+def get_image(image_prompt):
     model = replicate.models.get("stability-ai/stable-diffusion")
     image_output = model.predict(prompt=image_prompt)
     return image_output
