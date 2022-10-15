bc14351c286061b58e54cfa7da570303a82966ac
Maxski <ma.ad.jank@gmail.com>
2022-10-15 16:45:25 +0100

    update

---
+++ JP/AI dungeon/app/main.py
@@ -0,0 +1,18 @@
+from kobold_ai import generate_response, process_prompt
+from stable_diffusion import get_image
+
+def main(user_input):
+
+    correct_prompt = process_prompt(user_input)
+
+    kobold_response = generate_response(correct_prompt, "http://nasty-ravens-stare-104-198-213-42.loca.lt/api/v1/")
+
+    image_prompt = correct_prompt + kobold_response + " - van gogh"
+
+    print(correct_prompt + kobold_response)
+
+    image_url = get_image(image_prompt)
+
+    print(image_url)
+
+    return [image_url]
