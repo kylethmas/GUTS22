fbf99a7c51f93bc85d1da7ffb38e361677581c77
Maxski <ma.ad.jank@gmail.com>
2022-10-15 15:24:01 +0100

    Update test.py

--- JP/test.py
+++ JP/test.py
@@ -1,9 +1,31 @@
 import requests
 
+def generate_prompt(text_prompt):
 
-url = 'http://quick-kings-lose-34-134-245-36.loca.lt/api/v1/generate'
-myobj = {"prompt" : 'You ask "what you need that baby for"'}
+    url = 'http://moody-zoos-return-35-232-142-40.loca.lt/api/v1/generate'
+    request_body = {"prompt" : text_prompt}
 
-response = requests.post(url, json = myobj)
+    text_output = requests.post(url, json = request_body)
 
-print(response.json())
+    return text_output.json()['results'][0]['text'] #returns a string
+
+def process_prompt(text_prompt):
+
+    four_chars = text_prompt[:4]
+
+    starts_with_you = False
+
+    if not text_prompt[-1] == ".":
+        text_prompt = text_prompt + "."
+
+    if not four_chars.lower() == "you ":
+
+        if not text_prompt[0].islower():
+            text_prompt = text_prompt[0].lower() + text_prompt[1:]
+
+        text_prompt = "You " + text_prompt
+
+    return text_prompt
+    
+userInput=input()
+print(process_prompt(userInput))
