b136a9865071853eafb9b80a70016a7a0635cd59
kylethmas <kyle2010thomas@gmail.com>
2022-10-15 13:31:03 +0100

    reformat

--- JP/AI dungeon/app/app.py
+++ JP/AI dungeon/app/app.py
@@ -16,22 +16,25 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 socketio = SocketIO(app)
+
+
 @socketio.on('connect')
 def connect():
     session['sid'] = request.sid
-    
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return game() #render_template('game.html')
+        return game()  # render_template('game.html')
     return render_template('index.html')
-    
-@app.route("/", methods=['GET', 'POST'])   
+
+
+@app.route("/", methods=['GET', 'POST'])
 def game():
     model = replicate.models.get("stability-ai/stable-diffusion")
-    #this just takes us to the lil website - we dont like this
+    # this just takes us to the lil website - we dont like this
     output_url = model.predict(prompt="electric sheep, neon, synthwave")[0]
     print(output_url)
-    return render_template('game.html', user_image = output_url)
-    #webbrowser.open(output_url)
+    return render_template('game.html', user_image=output_url)
+    # webbrowser.open(output_url)
