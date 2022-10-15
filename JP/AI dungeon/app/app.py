bdc2eed5c81a56df236fd86aba5e715838371d0c
NiamhGillespie <2549880G@student.gla.ac.uk>
2022-10-15 14:38:03 +0100

    location and art style pages working

--- JP/AI dungeon/app/app.py
+++ JP/AI dungeon/app/app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, emit
 from uuid import uuid4
 from flask import session
 from flask_session import Session
-from flask import request
+from flask import request, url_for, redirect
 import replicate
 import webbrowser
 from PIL import Image
@@ -24,14 +24,30 @@ def connect():
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return game() #render_template('game.html')
+        return redirect(url_for('location'))
     return render_template('index.html')
     
-@app.route("/", methods=['GET', 'POST'])   
+@app.route("/location", methods=['GET', 'POST'])   
+def location():
+    if request.method == 'POST':
+        #print(request.form['location'])
+        print("I SHOULD BE MOVIN")
+        return redirect(url_for('art'))#, user_image = output_url)
+    return render_template('location.html')
+    
+@app.route("/art_style", methods=['GET', 'POST'])   
+def art():
+    if request.method == 'POST':
+        #print(request.form['art style'])
+        return redirect(url_for('game'))#, user_image = output_url)
+    return render_template('art_style.html')
+    
+@app.route("/game", methods=['GET', 'POST'])   
 def game():
-    model = replicate.models.get("stability-ai/stable-diffusion")
+    #model = replicate.models.get("stability-ai/stable-diffusion")
     #this just takes us to the lil website - we dont like this
-    output_url = model.predict(prompt="electric sheep, neon, synthwave")[0]
-    print(output_url)
-    return render_template('game.html', user_image = output_url)
+    #output_url = model.predict(prompt="electric sheep, neon, synthwave")[0]
+    #print(output_url)
+    #return render_template('game.html', user_image = output_url)
+    #return render_template('game.html')
     #webbrowser.open(output_url)
