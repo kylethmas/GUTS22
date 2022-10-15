5e53ae0479df656635bdd798ff489f975863d5f7
Hasan Faruk Arberk <62434912+hasanarberk@users.noreply.github.com>
2022-10-15 01:09:39 +0100

    Update stable-diffusion-test.py

--- JP/stable-diffusion-test.py
+++ JP/stable-diffusion-test.py
@@ -2,3 +2,5 @@ import replicate
 model = replicate.models.get("stability-ai/stable-diffusion")
 output = model.predict(prompt="")
 #print(output)
+
+#export REPLICATE_API_TOKEN=ec2408b84a5d6975bf214c713cd71c84d4c22a8b
